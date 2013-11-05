6599228f7670cf3f7ff4d3d3346f498f5942d3f9
Dirkjan Ochtman <dirkjan@ochtman.nl>
2013-11-05 13:39:29 +0100

    rename custom header, per RFC 6648

--- src/defines.h
+++ src/defines.h
@@ -7,7 +7,7 @@
 #define PERSONA_COOKIE_NAME "Persona"
 #define PERSONA_ISSUER_NOTE "persona-identity-issuer"
 #define PERSONA_SECRET_SIZE 1024
-#define PERSONA_ASSERTION_HEADER "X-Persona-Assertion"
+#define PERSONA_ASSERTION_HEADER "Persona-Assertion"
 #define PERSONA_END_PAGE "\n</script>\n</html>\n"
 
 #define ERRTAG "authnz_persona: "
